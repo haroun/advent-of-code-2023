936790fbf620b5aadb96b767055f82f0ed20188b
Harouna Traoré <haroun@users.noreply.github.com>
2023-12-06 10:02:03 +0100

    test cardinal

--- 01/main.ts
+++ 01/main.ts
@@ -1,8 +1,14 @@
-import { readLines } from 'https://deno.land/std/io/mod.ts'
-import * as path from 'https://deno.land/std/path/mod.ts'
+import { readLines } from 'https://deno.land/std@0.208.0/io/mod.ts'
+import * as path from 'https://deno.land/std@0.208.0/path/mod.ts'
 
 const filePath = path.join(Deno.cwd(), '01/input.txt')
 const reader = await Deno.open(filePath)
+// const cardinals = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ]
+
+const cardinalToDigitReplacer = (match, zero, one, two, three, four, five, six, seven, eight, nine) => '0123456789'
+const cardinalToDigit = (line) => {
+	return line.replace(/(zero)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/g, cardinalToDigitReplacer)
+}
 
 const getCalibrationValue = (line) => {
 	const digits = line.match(/[0-9]+/g).join('')
@@ -11,6 +17,7 @@ const getCalibrationValue = (line) => {
 
 let calibration = 0;
 for await (let line of readLines(reader)) {
+	console.log(cardinalToDigit(line))
 	calibration += getCalibrationValue(line)
 }
 
