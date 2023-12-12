4d3dfa19a54c434711ebb12d2fa210c7d6215bdd
Harouna Traoré <haroun@users.noreply.github.com>
2023-12-12 18:37:06 +0100

    1B

--- 01/main.ts
+++ 01/main.ts
@@ -3,11 +3,28 @@ import * as path from 'https://deno.land/std@0.208.0/path/mod.ts'
 
 const filePath = path.join(Deno.cwd(), '01/input.txt')
 const reader = await Deno.open(filePath)
-// const cardinals = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' ]
 
-const cardinalToDigitReplacer = (match, zero, one, two, three, four, five, six, seven, eight, nine) => '0123456789'
+const cardinalToDigitReplacer = (match, oneight, twone, threeight, fiveight, eightwo, eighthree, nineight, one, two, three, four, five, six, seven, eight, nine) => {
+	return (oneight && 18)
+		|| (twone && 21)
+		|| (threeight && 38)
+		|| (fiveight && 58)
+		|| (eightwo && 82)
+		|| (eighthree && 83)
+		|| (nineight && 98)
+		|| (one && 1)
+		|| (two && 2)
+		|| (three && 3)
+		|| (four && 4)
+		|| (five && 5)
+		|| (six && 6)
+		|| (seven && 7)
+		|| (eight && 8)
+		|| (nine && 9)
+		|| 0
+}
 const cardinalToDigit = (line) => {
-	return line.replace(/(zero)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/g, cardinalToDigitReplacer)
+	return line.replace(/(oneight)|(twone)|(threeight)|(fiveight)|(eightwo)|(eighthree)|(nineight)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)/g, cardinalToDigitReplacer)
 }
 
 const getCalibrationValue = (line) => {
@@ -17,8 +34,8 @@ const getCalibrationValue = (line) => {
 
 let calibration = 0;
 for await (let line of readLines(reader)) {
-	console.log(cardinalToDigit(line))
-	calibration += getCalibrationValue(line)
+	const asDigit = cardinalToDigit(line)
+	calibration += getCalibrationValue(asDigit)
 }
 
 console.log(calibration)
